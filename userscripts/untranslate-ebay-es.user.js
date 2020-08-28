980557cfaa0625740cbfd24168011e6d0561e339
Marcos Del Sol Vives <marcos@orca.pet>
2020-08-28 10:31:22 +0200

    Updated web

--- userscripts/untranslate-ebay-es.user.js
+++ userscripts/untranslate-ebay-es.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name        Untranslate eBay.es
 // @namespace   https://orca.pet
-// @version     1.0.1
+// @version     1.0.2
 // @author      Marcos Del Sol Vives <marcos@orca.pet>
 // @description Removes the shitty autotranslation from eBay.es ad titles
 // @homepage    https://orca.pet/userscripts/
 // @updateURL   https://orca.pet/userscripts/untranslate-ebay-es.meta.js
 // @downloadURL https://orca.pet/userscripts/untranslate-ebay-es.user.js
 // @supportURL  https://github.com/socram8888/orca.pet/issues
+// @match       https://www.ebay.es/b/*
 // @match       https://www.ebay.es/sch/*
 // @match       https://www.ebay.es/itm/*
 // @run-at      document-end
